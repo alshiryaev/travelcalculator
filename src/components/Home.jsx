741da2804f429e7c39e373dc729337c2b6b3b3a1
Yuriy Pinegin <che_zmoor@mail.ru>
2018-03-03 20:16:54 +0500

    Изменения ниочём

---
+++ src/components/Home.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+
+class Home extends Component {
+  render() {
+    return (
+        <p className="hq">Hello World</p>
+    );
+  }
+}
+
+export default Home;
