85e85a2d7a58507a2b92911b63c4bbaa3ea34e7a
Yuriy Pinegin <che_zmoor@mail.ru>
2018-03-04 03:24:38 +0500

    добавлен footer

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,38 +1,39 @@
 import React, { Component } from 'react';
 import './css/normalize.css';
 import './css/App.css';
-import {Route, Link } from 'react-router-dom';
+import { Route, Link } from 'react-router-dom';
 import routes from '../routes';
 
 class App extends Component {
   render() {
     return (
       <div>
-        <header>
-          <ul>
-            <li><Link to="/">Главная</Link></li>
-            <li><Link to="/Calculator">Калькулятор</Link></li>
-            <li><Link to="/About">О нас</Link></li>
-          </ul>
-        </header>
+        <div className="wrapper">
+          <header>
+            <ul>
+              <li><Link to="/">Главная</Link></li>
+              <li><Link to="/Calculator">Калькулятор</Link></li>
+              <li><Link to="/About">О нас</Link></li>
+            </ul>
+          </header>
 
-        {/* Здесь вставляется основной контент на каждой странице */}
+          {/* Здесь вставляется основной контент на каждой странице */}
+          <section>
+          {routes.map((route, index) => (
+            <Route
+              key={index}
+              path={route.path}
+              exact={route.exact}
+              component={route.component}
+            />
+          ))}
+          </section>
+          {/* Основной контент заканчивается */}
 
-        {routes.map((route, index) => (
-          <Route
-            key={index}
-            path={route.path}
-            exact={route.exact}
-            component={route.component}
-          />
-        ))}
-
-        {/* Основной контент заканчивается */}
-
-        <footer>
-          &copy; Yuretc & Alex Perm 2018
+          <footer>
+            &copy; Yuretc & Alex Perm 2018
         </footer>
-
+        </div>
       </div>
     );
   }
