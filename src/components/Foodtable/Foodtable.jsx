d16601fd179dcf62aa12f15f342e6d5f582d88f5
Yuriy Pinegin <che_zmoor@mail.ru>
2018-03-06 00:34:22 +0500

    добавлена вкладка Раскладка продуктов

---
+++ src/components/Foodtable/Foodtable.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class foodTable extends Component {
+  render() {
+    return (
+      <div>
+       <p>Приготовим, поедим</p>
+      </div>
+    );
+  }
+}
+
+export default foodTable;
--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,7 @@
 import Home from './components/Home/Home';
 import Calculator from './components/Calculator/Calculator';
 import About from './components/About/About';
+import foodTable from './components/Foodtable/Foodtable';
 
 const routes = [
     {
@@ -15,6 +16,10 @@ const routes = [
     {
         path:'/about',
         component:About
+    },
+    {
+        path:'/foodTable',
+        component:foodTable
     }
 ];
 
