85566fbf3bc3f53bbb660161d027f847ba9da158
Yuriy Pinegin <che_zmoor@mail.ru>
2018-03-06 02:18:35 +0500

    ref#1 начало вёрстки таблицы с продуктами, переделать таблицу

--- src/components/Foodtable/Foodtable.jsx
+++ src/components/Foodtable/Foodtable.jsx
@@ -1,10 +1,38 @@
 import React, { Component } from 'react';
+import './foodtable.css';
 
 class foodTable extends Component {
   render() {
     return (
       <div>
-       <p>Приготовим, поедим</p>
+        <div className="wrapper_table">
+          <p>Приготовим, поедим</p>
+          <div className="food__table">
+            <div className="food__table_row">
+              <div className="food__table_cell cell-1">Название продукта</div>
+              <div className="food__table_cell cell-2">Масса/объём</div>
+            </div>
+            <div className="food__table_row">Завтрак 1 день</div>
+            <div className="food__table_row">
+              
+            </div>
+            <div className="food__table_row">
+              
+            </div>
+            <div className="food__table_row">
+              
+            </div>
+            <div className="food__table_row">
+              
+            </div>
+            <div className="food__table_row">
+              
+            </div>
+            <div className="food__table_row">
+              
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
