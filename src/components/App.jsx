b0047f4aee6285952edfec3b58b1b620dd723d5a
Yuriy Pinegin <che_zmoor@mail.ru>
2018-03-18 01:00:39 +0500

    удалил лушний блок

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,21 +7,20 @@ import routes from '../routes';
 class App extends Component {
   render() {
     return (
-      <div>
-        <div className="wrapper">
-          <header>
-            <nav className="navbar">
+      <div className="wrapper">
+        <header>
+          <nav className="navbar">
             <ul>
-                <li><NavLink exact className="nav-link" activeClassName="active" to={"/"}>Главная</NavLink ></li>
-                <li><NavLink className="nav-link" activeClassName="active" to={"/calculator"}>Калькулятор</NavLink ></li>
-                <li><NavLink className="nav-link" activeClassName="active" to={"/products"}>Продукты</NavLink ></li>
-                <li><NavLink className="nav-link" activeClassName="active" to={"/about"}>О нас</NavLink ></li>               
+              <li><NavLink exact className="nav-link" activeClassName="active" to={"/"}>Главная</NavLink ></li>
+              <li><NavLink className="nav-link" activeClassName="active" to={"/calculator"}>Калькулятор</NavLink ></li>
+              <li><NavLink className="nav-link" activeClassName="active" to={"/products"}>Продукты</NavLink ></li>
+              <li><NavLink className="nav-link" activeClassName="active" to={"/about"}>О нас</NavLink ></li>
             </ul>
-            </nav>
-          </header>
+          </nav>
+        </header>
 
-          {/* Здесь вставляется основной контент на каждой странице */}
-          <section>
+        {/* Здесь вставляется основной контент на каждой странице */}
+        <section>
           {routes.map((route, index) => (
             <Route
               key={index}
@@ -30,22 +29,22 @@ class App extends Component {
               component={route.component}
             />
           ))}
-          </section>
-          {/* Основной контент заканчивается */}
+        </section>
+        {/* Основной контент заканчивается */}
 
-          <footer>
-            <div className="social">
+        <footer>
+          <div className="social">
             Мы в социальных сетях:
               <ul>
-                <li><a className="href-box" href="https://vk.com/alexpl"> </a><div className="social-links__logo"></div>Alexey Shiryaev</li>
-                <li><a className="href-box" href="https://vk.com/id7339163"> </a><div className="social-links__logo"></div>Yuriy Pinegin</li>
-                {/* <li><a href="https://vk.com/ikuchev">Dmitriy Kuchev</a></li> */}
-              </ul>
-            </div>
-            {/* <div>&copy; Yuretc & Alex Perm 2018</div> */}
+              <li><a className="href-box" href="https://vk.com/alexpl"> </a><div className="social-links__logo"></div>Alexey Shiryaev</li>
+              <li><a className="href-box" href="https://vk.com/id7339163"> </a><div className="social-links__logo"></div>Yuriy Pinegin</li>
+              {/* <li><a href="https://vk.com/ikuchev">Dmitriy Kuchev</a></li> */}
+            </ul>
+          </div>
+          {/* <div>&copy; Yuretc & Alex Perm 2018</div> */}
         </footer>
-        </div>
       </div>
+
     );
   }
 }
