278b15a48673c8a702275e10374d3fc0ba404513
Alexey Shiryaev <alexpl_07@inbox.ru>
2018-06-23 23:16:02 +0500

    Работа с БД PostgreSQL. Миграции

--- server/server.js
+++ server/server.js
@@ -3,18 +3,20 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 5000;
 const fakeProducts = require('./testProducts').products;
+const db = require('./db').db;
 
-
+// api routes
 app.get('/api/products', (req, res) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'origin, content-type, accept');
     res.send(fakeProducts);
 });
 
+// dd connection test
+db.test();
+
 if (process.env.NODE_ENV === 'production') {
-    // Serve any static files
     app.use(express.static(path.join(__dirname, 'client/build')));
-    // Handle React routing, return all requests to React app
     app.get('*', function (req, res) {
         res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
     });
