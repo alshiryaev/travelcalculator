4900cd735beee8b99893590570ff39eb260ed581
Alexey Shiryaev <alexpl_07@inbox.ru>
2018-09-27 21:25:44 +0500

    hamburger

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import './css/App.css';
+import './App.css';
 import { Route, NavLink } from 'react-router-dom';
 import routes from '../routes';
 import { HashRouter } from 'react-router-dom';
@@ -9,36 +9,40 @@ import { HashRouter } from 'react-router-dom';
 export default class App extends Component {
   render() {
     return (
-        <HashRouter>
-          <div className="wrapper">
-            <header className="header">         
-              <nav>
-                <ul className="nav-menu">
-                  <li className="nav-menu__item"><NavLink exact className="menu-link" activeClassName="menu-link_active" to={"/"}>Главная</NavLink ></li>
-                  <li className="nav-menu__item"><NavLink className="menu-link" activeClassName="menu-link_active" to={"/calculator"}>Калькулятор</NavLink ></li>
-                  <li className="nav-menu__item"><NavLink className="menu-link" activeClassName="menu-link_active" to={"/products"}>Таблица БЖУ</NavLink ></li>
-                  <li className="nav-menu__item"><NavLink className="menu-link" activeClassName="menu-link_active" to={"/recipes"}>Рецепты</NavLink ></li>
-                  <li className="nav-menu__item"><NavLink className="menu-link" activeClassName="menu-link_active" to={"/about"}>О нас</NavLink ></li>
-                </ul>
-              </nav>
-            </header>
-            
-            <section className="main-section">
-           
-              {routes.map((route, index) => (
-                <Route
-                  key={index}
-                  path={route.path}
-                  exact={route.exact}
-                  component={route.component} />
-              ))}
-            </section>
+      <HashRouter>
+        <div className="wrapper">
+          <header className="header">            
+            <div className="logo">
+              <NavLink className="logo__link" to={"/"}>TRAVELCALCULATOR</NavLink >
+            </div>
+            <input className="hamburger__checkbox" type="checkbox" hidden id="__hamb__button"/>     
+            <label className="hamburger hamburger_clickable" htmlFor="__hamb__button">                      
+                 <div className="hamburger__button"></div>
+            </label>
+            <ul className="nav-menu">
+              <li className="nav-menu__item"><NavLink className="menu-link" activeClassName="menu-link_active" to={"/calculator"}>Калькулятор</NavLink ></li>
+              <li className="nav-menu__item"><NavLink className="menu-link" activeClassName="menu-link_active" to={"/products"}>Таблица БЖУ</NavLink ></li>
+              <li className="nav-menu__item"><NavLink className="menu-link" activeClassName="menu-link_active" to={"/recipes"}>Рецепты</NavLink ></li>
+              <li className="nav-menu__item"><NavLink className="menu-link" activeClassName="menu-link_active" to={"/about"}>О нас</NavLink ></li>
+            </ul>
+          </header>
 
-            <footer>
-            </footer>
-          </div>
+          <section className="main-section">
 
-        </HashRouter>
+            {routes.map((route, index) => (
+              <Route
+                key={index}
+                path={route.path}
+                exact={route.exact}
+                component={route.component} />
+            ))}
+          </section>
+
+          <footer>
+          </footer>
+        </div>
+
+      </HashRouter>
     );
   }
 }
