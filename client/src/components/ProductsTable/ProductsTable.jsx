ca6f1181025d62d1b8a94ed20d97cc4598b51958
Yuriy Pinegin <che_zmoor@mail.ru>
2018-10-03 13:34:56 +0500

    Переделал таблицу и немного основной макет

--- client/src/components/ProductsTable/ProductsTable.jsx
+++ client/src/components/ProductsTable/ProductsTable.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import './productTable.css';
-import Table from '@material-ui/core/Table';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
-import TableCell from '@material-ui/core/TableCell';
-import TableBody from '@material-ui/core/TableBody';
+// import Table from '@material-ui/core/Table';
+// import TableHead from '@material-ui/core/TableHead';
+// import TableRow from '@material-ui/core/TableRow';
+// import td from '@material-ui/core/td';
+// import TableBody from '@material-ui/core/TableBody';
 import Paper from '@material-ui/core/Paper';
 import IconButton from '@material-ui/core/IconButton';
 import DeleteIcon from '@material-ui/icons/Delete';
@@ -14,41 +14,41 @@ const ProductTable = props => {
   const { isAdmin, products, editProduct = f => f, deleteProduct = f => f } = props;
   return (
     <Paper>
-      <Table>
-        <TableHead>
-          <TableRow>
-            <TableCell>Название продукта</TableCell>
-            <TableCell>Белки, г</TableCell>
-            <TableCell>Жиры, г</TableCell>
-            <TableCell>Углеводы, г</TableCell>
-            <TableCell>Калорийность, ккал</TableCell>
+      <table>
+        <thead>
+          <tr>
+            <td>Название продукта</td>
+            <td>Белки, г</td>
+            <td>Жиры, г</td>
+            <td>Углеводы, г</td>
+            <td>Калорийность, ккал</td>
             {(isAdmin) ?
-              <TableCell>Управление</TableCell> : <TableCell></TableCell>}
+              <td class="forAdmin">Управление</td> : <td  class="forAdmin"></td>}
 
-          </TableRow>
-        </TableHead>
-        <TableBody>
+          </tr>
+        </thead>
+        <tbody>
           {products.map((product, index) => {
             return (
-              <TableRow key={index} >
-                <TableCell>{product.name}</TableCell>
-                <TableCell>{product.protein}</TableCell>
-                <TableCell>{product.fat}</TableCell>
-                <TableCell>{product.carbohydrates}</TableCell>
-                <TableCell>{product.calories}</TableCell>
+              <tr key={index} >
+                <td>{product.name}</td>
+                <td>{product.protein}</td>
+                <td>{product.fat}</td>
+                <td>{product.carbohydrates}</td>
+                <td>{product.calories}</td>
                 {isAdmin ?
-                  <TableCell>
+                  <td  class="forAdmin">
                     <IconButton title="Удалить" onClick={() => deleteProduct(product)}>
                       <DeleteIcon/>
                     </IconButton>
                     <IconButton title="Редактировать" onClick={() => editProduct(product)}>
                       <EditIcon/>
                     </IconButton>
-                  </TableCell> : <TableCell></TableCell>}
-              </TableRow>)
+                  </td> : <td class="forAdmin"></td>}
+              </tr>)
           })}
-        </TableBody>
-      </Table>
+        </tbody>
+      </table>
     </Paper>
   )
 }
