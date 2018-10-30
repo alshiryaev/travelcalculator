4f2734458ddd7deedc966b0906842a83aed29fd7
Alexey Shiryaev <alexpl_07@inbox.ru>
2018-10-30 20:48:50 +0500

    fix dependencies

--- server.js
+++ server.js
@@ -34,5 +34,5 @@ app.put('/api/products', jsonParser, (req, res) => {
 app.use(express.static(path.join(__dirname, 'client/build')));
 app.get('*', function (req, res) {
     res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-});y=
+});
 app.listen(port, () => console.log(`Listening on port ${port}`));
