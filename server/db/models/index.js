fca1acd1b04cbae63b826b6f6316e88aa8bcb873
Alexey Shiryaev <alexpl_07@inbox.ru>
2018-11-17 20:18:04 +0500

    config

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -13,9 +13,9 @@ var db        = {};
 
 
 if (config.use_env_variable) {
-  var sequelize = new Sequelize(process.env[config.use_env_variable]);
+  var sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
-  var sequelize = new Sequelize(config.url);
+  var sequelize = new Sequelize(config.url, config);
 }
 
 fs
