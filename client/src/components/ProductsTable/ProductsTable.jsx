733205e8bc04da8f8d3560a81b324cafd6a5806f
Alexey Shiryaev <alexpl_07@inbox.ru>
2019-01-02 00:22:54 +0500

    changed styles in admin

--- client/src/components/ProductsTable/ProductsTable.jsx
+++ client/src/components/ProductsTable/ProductsTable.jsx
@@ -1,9 +1,6 @@
 import React from 'react';
 import './productTable.css';
 import Paper from '@material-ui/core/Paper';
-import IconButton from '@material-ui/core/IconButton';
-import DeleteIcon from '@material-ui/icons/Delete';
-import EditIcon from '@material-ui/icons/Build';
 
 const ProductTable = props => {
   const { isAdmin, products, editProduct = f => f, deleteProduct = f => f } = props;
@@ -16,9 +13,9 @@ const ProductTable = props => {
             <td>Белки, г</td>
             <td>Жиры, г</td>
             <td>Углеводы, г</td>
-            <td style={isAdmin ? {borderTopRightRadius:'0'} : {borderTopRightRadius:'5px'}}>Калорийность, ккал</td>
+            <td style={isAdmin ? { borderTopRightRadius: '0' } : { borderTopRightRadius: '5px' }}>Калорийность, ккал</td>
 
-            <td style={isAdmin ? {display:'table-cell'} : {display: 'none'}}>Управление</td>
+            <td style={isAdmin ? { display: 'table-cell' } : { display: 'none' }}>Управление</td>
 
           </tr>
         </thead>
@@ -32,13 +29,9 @@ const ProductTable = props => {
                 <td>{product.carbohydrates}</td>
                 <td>{product.calories}</td>
 
-                <td style={isAdmin ? {display:'block'} : {display: 'none'}}>
-                  <IconButton title="Удалить" onClick={() => deleteProduct(product)}>
-                    <DeleteIcon />
-                  </IconButton>
-                  <IconButton title="Редактировать" onClick={() => editProduct(product)}>
-                    <EditIcon />
-                  </IconButton>
+                <td style={isAdmin ? { display: 'block' } : { display: 'none' }}>
+                  <button className="delete-button" onClick={() => deleteProduct(product)}>X</button>
+                  <button className="edit-button" onClick={() => editProduct(product)}>E</button>
                 </td>
               </tr>)
           })}
