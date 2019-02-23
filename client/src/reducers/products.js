cff1a098be3fcccbe1935a5dcb339f5d1c73f94a
Alexey Shiryaev <alexpl_07@inbox.ru>
2019-02-23 14:47:55 +0500

    fixed getProducts()

--- client/src/reducers/products.js
+++ client/src/reducers/products.js
@@ -2,36 +2,36 @@ import * as p from '../actions/products';
 import * as filter from '../actions/filter';
 
 const defaultState = {
-    items: [],
-    filter: ''
+  items: [],
+  filter: '',
 };
 
 export const products = (state = defaultState, action) => {
-    switch (action.type) {
-        case p.REQUEST_PRODUCTS:
-            return state;
-        case p.RECEIVE_PRODUCTS:
-            return {
-                ...state,
-                items: action.products
-            }
-        case filter.CHANGE_QUERY:
-            return {
-                ...state,
-                filter: action.query
-            }
-        case filter.CLEAR_QUERY:
-            return {
-                ...state,
-                filter: ''
-            }
-        case p.ADD_PRODUCT:
-            const items = state.items.concat(action.product)
-            return {
-                ...state,
-                items
-            }
-        default:
-            return state;
-    }
-}
+  switch (action.type) {
+    case p.REQUEST_PRODUCTS:
+      return state;
+    case p.RECEIVE_PRODUCTS:
+      return {
+        ...state,
+        items: action.payload.products,
+      };
+    case filter.CHANGE_QUERY:
+      return {
+        ...state,
+        filter: action.query,
+      };
+    case filter.CLEAR_QUERY:
+      return {
+        ...state,
+        filter: '',
+      };
+    case p.ADD_PRODUCT:
+      const items = state.items.concat(action.payload.product);
+      return {
+        ...state,
+        items,
+      };
+    default:
+      return state;
+  }
+};
