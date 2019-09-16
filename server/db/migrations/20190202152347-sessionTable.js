1c080c310bce3eccfbae8f589af91205af69edb7
Alexey Shiryaev <alexpl_07@inbox.ru>
2019-09-17 00:37:13 +0500

    fix migration

--- server/db/migrations/20190202152347-sessionTable.js
+++ server/db/migrations/20190202152347-sessionTable.js
@@ -1,6 +1,6 @@
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    queryInterface.sequelize.query(
+    return queryInterface.sequelize.query(
       'CREATE TABLE "session" ("sid" varchar NOT NULL COLLATE "default", "sess" json NOT NULL,  "expire" timestamp(6) NOT NULL )  WITH (OIDS=FALSE); ALTER TABLE "session" ADD CONSTRAINT "session_pkey" PRIMARY KEY ("sid") NOT DEFERRABLE INITIALLY IMMEDIATE;'
     );
   },
