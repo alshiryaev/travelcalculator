5726babac086c4b30c319edc126d0dc690c0648d
Alexey Shiryaev <alexpl_07@inbox.ru>
2020-01-07 02:38:16 +0500

    Changes styles

--- client/src/services/authService.js
+++ client/src/services/authService.js
@@ -3,10 +3,7 @@ const cancelToken = axios.CancelToken;
 const source = cancelToken.source();
 
 export default class AuthService {
-  _isAuth = false;
-
   loginUrl = process.env.REACT_APP_API_URL + '/login';
-  isAuthUrl = process.env.REACT_APP_API_URL + '/isAuth';
 
   async login(username, password) {
     return axios.post(this.loginUrl, null, {
